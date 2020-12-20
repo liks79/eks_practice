d156f0b1a35a56773c5c344583db4e14b143a79e
Sungshik Jou <liks79@gmail.com>
2020-12-20 17:31:09 +0900

    initial commit for Multiply service

---
+++ services/multiply/service/database/models.py
@@ -0,0 +1,32 @@
+
+from sqlalchemy import DateTime, Integer, String
+from datetime import datetime
+from service import db
+
+
+class MultiplyResult(db.Model):
+    """
+    Database Model class for plus_result table
+    """
+    __tablename__ = 'plus_result'
+
+    id = db.Column(Integer, primary_key=True)
+    operand_1 = db.Column(Integer)
+    operand_2 = db.Column(Integer)
+    operator = db.Column(String(1), default='+')
+    result = db.Column(Integer)
+    date = db.Column(DateTime, unique=False, default=datetime)
+
+    def __init__(self, operand_1, operand_2):
+        self.operand_1 = operand_1
+        self.operand_2 = operand_2
+
+    def __repr__(self):
+        return '<%r %r %r>' % (self.__tablename__, self.operand_1, self.operand_2)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'operand_1': self.operand_1,
+            'operand_2': self.operand_2
+        }
